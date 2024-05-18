a11cf452fcc603247c2c40b2396aa73e800d3092
estiven0425 <estivenmoto6@gmail.com>
2024-05-18 00:20:44 -0500

    Animación de formularios

--- code/shair-react/src/utils/controller/FormularioRenderizado.jsx
+++ code/shair-react/src/utils/controller/FormularioRenderizado.jsx
@@ -1,34 +1,82 @@
-import React from "react";
-import { useContext } from "react";
+import React, { useContext } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 import FormularioContexto from "../../context/FormularioContexto";
 import FormularioInicio from "../../components/forms/FormularioInicio";
 import FormularioSuperadministrador from "../../components/forms/FormularioSuperadministrador";
 import FormularioAdministrador from "../../components/forms/FormularioAdministrador";
+import '../styles/transicion.css';
+
+const animacionDerecha = {
+    initial: {
+        opacity: 0,
+        rotateY: 90
+    },
+    in: {
+        opacity: 1,
+        rotateY: 0
+    },
+    out: {
+        opacity: 0,
+        rotateY: 90
+    }
+};
+
+const animacionIzquierda = {
+    initial: {
+        opacity: 0,
+        rotateY: -90
+    },
+    in: {
+        opacity: 1,
+        rotateY: 0
+    },
+    out: {
+        opacity: 0,
+        rotateY: -90
+    }
+};
+
+const transicionPagina = {
+    type: "tween",
+    duration: 0.25
+};
 
 function FormularioRenderizado() {
-    const formulario = useContext(FormularioContexto);
+    const { formulario } = useContext(FormularioContexto);
 
     let formularioRenderizado;
 
-    switch (formulario.formulario) {
+    switch (formulario) {
         case 0:
-            formularioRenderizado = <FormularioInicio />
+            formularioRenderizado = <FormularioInicio />;
             break;
         case 1:
-            formularioRenderizado = <FormularioSuperadministrador />
+            formularioRenderizado = <FormularioSuperadministrador />;
             break;
         case 2:
-            formularioRenderizado = <FormularioAdministrador />
+            formularioRenderizado = <FormularioAdministrador />;
             break;
         default:
-            formularioRenderizado = <FormularioInicio />
+            formularioRenderizado = <FormularioInicio />;
     }
 
+    const varianteAnimacion = formulario == 0 ? animacionIzquierda : animacionDerecha;
+
     return (
-        <>
-            {formularioRenderizado}
-        </>
-    )
+        <AnimatePresence mode="wait">
+            <motion.div
+                key={formulario}
+                initial="initial"
+                animate="in"
+                exit="out"
+                variants={varianteAnimacion}
+                transition={transicionPagina}
+                id="formularioTransicion"
+            >
+                {formularioRenderizado}
+            </motion.div>
+        </AnimatePresence>
+    );
 }
 
 export default FormularioRenderizado;
